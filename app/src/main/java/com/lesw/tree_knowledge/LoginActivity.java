c5ad099f6d2a1141235ce3cfb5e1628f559c8584
Daniel Silva Ferreira Bruno <dsfbstudent@yahoo.com.br>
2018-11-04 16:26:13 -0200

    Arrumando a versão para 1.1, e tentando corrigir uma das issues solicitadas...

--- app/src/main/java/com/lesw/tree_knowledge/LoginActivity.java
+++ app/src/main/java/com/lesw/tree_knowledge/LoginActivity.java
@@ -50,7 +50,7 @@ public class LoginActivity extends AppCompatActivity {
                 // Start the Signup activity
                 Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
                 startActivityForResult(intent, REQUEST_SIGNUP);
-                finish();
+                //finish();
                 overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
             }
         });
