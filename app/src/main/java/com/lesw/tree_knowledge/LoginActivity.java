9b4b1726fd5fd49e6581842b8afcd77ec904daa2
Daniel Silva Ferreira Bruno <dsfbstudent@yahoo.com.br>
2018-11-18 20:48:56 -0200

    Primeiro Checkpoint para a noite de 18/11/2018.

--- app/src/main/java/com/lesw/tree_knowledge/LoginActivity.java
+++ app/src/main/java/com/lesw/tree_knowledge/LoginActivity.java
@@ -115,7 +115,7 @@ public class LoginActivity extends AppCompatActivity {
 
                 // TODO: Implement successful signup logic here
                 // By default we just finish the Activity and log them in automatically
-                this.finish();
+                // this.finish();
             }
         }
     }
